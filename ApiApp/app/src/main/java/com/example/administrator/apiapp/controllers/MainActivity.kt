ebed6acca663cc95724a98a9ba4863f226135739
Some idiot <yujhyujhgujkh>
2018-10-31 14:23:42 +0000

    Progress of the Assignment
    Already started developing. Without comments yet .

---
+++ ApiApp/app/src/main/java/com/example/administrator/apiapp/controllers/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.administrator.apiapp.controllers
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.administrator.apiapp.R
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
